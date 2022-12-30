154db5728661a48eef6eb430aaee98d9cd774959
hmbadhon2 <hmmizanurrahmanbadhon@gmail.com>
2022-12-30 19:00:25 +0600

    try about data

--- index.js
+++ index.js
@@ -17,12 +17,40 @@ console.log(uri)
 async function run(){
     try{
             const usersCollection = client.db("Social-Say").collection("users")
+            const userInfoCollection = client.db("Social-Say").collection("userInfo")
             const postCollection = client.db("Social-Say").collection("posts")
+
+            app.get('/users', async(req,res)=>{
+                const query = {}
+                const users = await usersCollection.find(query).toArray()
+                res.send(users)
+            })
+            app.get('/users/:id', async(req,res)=>{
+                const id = req.params.id;
+                const query = {_id:Object(id)}
+                const users = await usersCollection.findOne(query)
+                res.send(users)
+            })
+            // app.put('/users', async(req,res)=>{
+            //     const email = req.params.email
+            //     const updateUser = req.body
+            //     const filter ={email:email}
+            //     const updatedDoc ={
+            //         $set:{"name":"updateUser.name","email":"updateUser.email","address":"updateUser.address","education":"updateUser.education"}
+            //     }
+            //     const result = await usersCollection.updateOne(updatedDoc,filter)
+            //     res.send(result)
+            // })
 
             app.post('/users', async(req,res)=>{
                 const user = req.body
                 const result = await usersCollection.insertOne(user)
                 res.send(user)
+            })
+            app.post('/usersInfo', async(req,res)=>{
+                const updateUser = req.body
+                const result = await userInfoCollection.insertOne(updateUser)
+                res.send(updateUser)
             })
 
             app.get('/post', async(req,res)=>{
